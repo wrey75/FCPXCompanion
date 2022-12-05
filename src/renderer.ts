b41d7f4710316d32bf6402bb1e0c82cb38b2d3d5
William Rey <wrey75@gmail.com>
2022-12-05 23:16:27 +0100

    fixed bootstrap css loading

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,7 +26,6 @@
  * ```
  */
 
-import 'bootstrap';
-import './scss/app.scss';
+ import './css/bootstrap.css';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
--- webpack.rules.ts
+++ webpack.rules.ts
@@ -1,20 +1,20 @@
-import type { ModuleOptions } from 'webpack';
+import type { ModuleOptions } from "webpack";
 
-export const rules: Required<ModuleOptions>['rules'] = [
+export const rules: Required<ModuleOptions>["rules"] = [
   // Add support for native node modules
   {
     // We're specifying native_modules in the test because the asset relocator loader generates a
     // "fake" .node file which is really a cjs file.
     test: /native_modules[/\\].+\.node$/,
-    use: 'node-loader',
+    use: "node-loader",
   },
   {
     test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: '@vercel/webpack-asset-relocator-loader',
+      loader: "@vercel/webpack-asset-relocator-loader",
       options: {
-        outputAssetBase: 'native_modules',
+        outputAssetBase: "native_modules",
       },
     },
   },
@@ -22,38 +22,29 @@ export const rules: Required<ModuleOptions>['rules'] = [
     test: /\.tsx?$/,
     exclude: /(node_modules|\.webpack)/,
     use: {
-      loader: 'ts-loader',
+      loader: "ts-loader",
       options: {
         transpileOnly: true,
       },
     },
   },
   {
-    test: /\.(scss)$/,
-    use: [{
-      loader: 'style-loader', // inject CSS to page
-    }, {
-      loader: 'css-loader', // translates CSS into CommonJS modules
-    }, {
-      loader: 'postcss-loader', // Run post css actions
-      options: {
-        plugins: function () { // post css plugins, can be exported to postcss.config.js
-          return [
-            require('precss'),
-            require('autoprefixer')
-          ];
-        }
-      }
-    }, {
-      loader: 'sass-loader' // compiles Sass to CSS
-    }]
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      "style-loader",
+      // Translates CSS into CommonJS
+      "css-loader",
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
   },
   {
     test: /\.(png|jpe?g|gif|ico|svg)$/, // We will handle of these file extensions
     use: [
       {
         loader: "file-loader",
-      }
-    ]
-  }
+      },
+    ],
+  },
 ];
